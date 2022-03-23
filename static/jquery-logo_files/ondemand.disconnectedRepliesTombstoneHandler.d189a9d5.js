413a440f273607ae748bf528d67961ece6adf9d2
Mike North <michael.l.north@gmail.com>
2022-03-23 08:55:23 -0500

    remove junk

--- static/jquery-logo_files/ondemand.disconnectedRepliesTombstoneHandler.d189a9d5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[299],{"08qV":function(t,e,a){"use strict";a.r(e);var c=a("ezF+"),n=a("VPAj"),o=a("3XMw"),r=a.n(o),s=a("xM7j");const p=r.a.g0b48670,i=r.a.i859a9d3;e.default=c.g({component:s.a,createProps:({entry:t})=>({children:p,ctaLink:"https://support.twitter.com/articles/370610",ctaText:i}),shouldDisplayBorder:Object(n.a)(!1)})}}]);
-//# sourceMappingURL=https://ton.local.twitter.com/responsive-web-internal/sourcemaps/client-web/ondemand.disconnectedRepliesTombstoneHandler.d189a9d5.js.map
