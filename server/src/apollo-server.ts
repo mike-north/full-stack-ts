dfad9e844d7d6689232d75caa8907d12f758f4f2
Mike North <michael.l.north@gmail.com>
2022-03-23 11:25:32 -0500

    02-hello-resolver

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -5,11 +5,10 @@ import { Server } from 'http';
 import Db from './db';
 
 export async function createApolloServer(
-  _db: Db,
+  db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-
   const typeDefs = gql`
     type Query {
       currentUser: User!
@@ -32,8 +31,29 @@ export async function createApolloServer(
     }
   `;
 
+  const resolvers = {
+    Query: {
+      currentUser: () => {
+        return {
+          id: '123',
+          name: 'John Doe',
+          handle: 'johndoe',
+          coverUrl: '',
+          avatarUrl: '',
+          createdAt: '',
+          updatedAt: '',
+        };
+      },
+      suggestions: () => {
+        return [];
+      },
+    },
+  };
+
   const server = new ApolloServer({
     typeDefs,
+    resolvers,
+    context: () => ({ db }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   await server.start();
