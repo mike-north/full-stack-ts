a07a281f710a8db4e0f02db1506235d906b0df9b
Mike North <michael.l.north@gmail.com>
2022-03-23 12:47:54 -0500

    06-data-from-db

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,34 +1,17 @@
+import { TwitterResolverContext } from '../resolvers';
 import { QueryResolvers } from '../resolvers-types.generated';
 
-const TwitterQueryResolver: QueryResolvers = {
-  currentUser: () => {
-    return {
-      id: '123',
-      name: 'John Doe',
-      handle: 'johndoe',
-      coverUrl: '',
-      avatarUrl: '',
-      createdAt: '',
-      updatedAt: '',
-    };
+const TwitterQueryResolver: QueryResolvers<TwitterResolverContext> = {
+  currentUser: (_, __, { db }) => {
+    const [firstUser] = db.getAllUsers();
+    if (!firstUser)
+      throw new Error(
+        'currentUser was requested, but there are no users in the database'
+      );
+    return firstUser;
   },
-  suggestions: () => {
-    return [
-      {
-        name: 'TypeScript Project',
-        handle: 'TypeScript',
-        avatarUrl: 'http://localhost:3000/static/ts-logo.png',
-        reason: 'Because you follow @MichaelLNorth',
-        id: '1',
-      },
-      {
-        name: 'jQuery',
-        handle: 'jquery',
-        avatarUrl: 'http://localhost:3000/static/jquery-logo.jpeg',
-        reason: 'Because you follow @FrontendMasters',
-        id: '2',
-      },
-    ];
+  suggestions: (_, __, { db }) => {
+    return db.getAllSuggestions();
   },
 };
 export default TwitterQueryResolver;
