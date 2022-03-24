7eff56d92e1ce443f23d893ece58f5712fc8d965
Mike North <michael.l.north@gmail.com>
2022-03-24 14:43:38 -0500

    create a new tweet

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,6 +1,7 @@
 import Query from './resolvers/Query'
 import User from './resolvers/User'
 import Tweet from './resolvers/Tweet'
+import Mutation from './resolvers/Mutation'
 import { Resolvers } from "./resolvers-types.generated"
 import Db, { DbTweet, DbUser } from "./db"
 
@@ -14,6 +15,7 @@ export interface TwitterResolverContext {
 const resolvers: Resolvers<TwitterResolverContext> = {
   Query,
   User,
+  Mutation,
   Tweet
 };
 
---
+++ server/src/resolvers/Mutation.ts
@@ -0,0 +1,25 @@
+import { tweetTransform } from '../transforms';
+import { TwitterResolverContext } from '../resolvers';
+import { MutationResolvers } from '../resolvers-types.generated';
+
+const mutationTwitterResolver: MutationResolvers<TwitterResolverContext> = {
+
+  async createTweet(_parent, args, { dbTweetCache, db }) {
+    const { body, userId } = args;
+    const dbTweet = await db.createTweet({
+      message: body,
+      userId,
+    });
+    const dbTweetMap = (dbTweetCache ||= {});
+    dbTweetMap[dbTweet.id] = dbTweet;
+    const dbAuthor = db.getUserById(userId);
+    if (!dbAuthor) throw new Error(`No author found for ${userId}`);
+    return Object.assign(
+      tweetTransform(dbTweet),
+      { author: dbAuthor },
+    );
+  },
+
+};
+
+export default mutationTwitterResolver;
