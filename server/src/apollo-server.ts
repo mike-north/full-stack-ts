1fba5d10d67245386a7e1e612ec61237470bbb37
Cameron Luo <cameronluo@gatech.edu>
2022-08-25 23:27:14 -0700

    Add resolver

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -1,14 +1,30 @@
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core"
-import {
-  ApolloServer,
-  ExpressContext,
-  gql,
-} from "apollo-server-express"
-import * as express from "express"
-import { Server } from "http"
-import Db from "./db"
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer, ExpressContext, gql } from 'apollo-server-express';
+import * as express from 'express';
+import { Server } from 'http';
+import Db from './db';
+
+const resolvers = {
+  Query: {
+    currentUser: () => {
+      return {
+        id: '123',
+        name: 'John Doe',
+        handle: 'johndoe',
+        coverUrl: '',
+        avatarUrl: '',
+        createdAt: '',
+        updatedAt: '',
+      };
+    },
+    suggestions: () => {
+      return [];
+    },
+  },
+};
+
 export async function createApolloServer(
-  _db: Db,
+  db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
@@ -32,14 +48,14 @@ export async function createApolloServer(
       avatarUrl: String!
       reason: String!
     }
-  `
+  `;
   const server = new ApolloServer({
     typeDefs,
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-    ],
-  })
-  await server.start()
-  server.applyMiddleware({ app })
-  return server
+    resolvers,
+    context: () => ({ db }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  return server;
 }
